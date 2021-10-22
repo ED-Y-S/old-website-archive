c9b48d5e4d9dcaa27f74d722e564e59bd249b7ba
Eddie-Shi <edward.shi1221@gmail.com>
2021-10-21 21:21:59 -0700

    Updates

---
+++ hw_02/steam_pop.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime, timedelta
+from matplotlib import dates as mpl_dates
+plt.style.use('seaborn')
+
+data = pd.read_csv('hw_02/steam_population/chart.csv')
+data["DateTime"] = pd.to_datetime(data["DateTime"])
+data.sort_values("DateTime", inplace=True)
+date_time = data["DateTime"]
+users = data["Users"]
+in_game = data["In-Game"]
+plt.plot_date(date_time, users, linestyle='solid')
+plt.plot_date(date_time, in_game, linestyle='solid')
+plt.title('Steam Weekly Activities')
+plt.xlabel('Date')
+plt.ylabel('Online Popution (in Tens of Millions)')
+plt.show()
