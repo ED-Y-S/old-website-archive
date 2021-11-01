47b110fb3bf10e1dfeec287797b69c66c3982294
Eddie-Shi <edward.shi1221@gmail.com>
2021-11-01 15:54:41 -0700

    Update

--- week_07/lab_07.py
+++ week_07/lab_07.py
@@ -2,15 +2,15 @@ import re
 
 
 # Read in the file and write into a new file
-d = open('week_07/dracula.html', 'r')
-i = open('week_07/izibicki.html', 'w')
+d = open('week_07/dracula.html', 'r', encoding = 'UTF-8')
+i = open('week_07/izbicki.html', 'w', encoding = 'UTF-8')
 
 
 # will print 'axample atring'
 
 # Check the words and prepare replacing words
 check_words = ('D&nbsp;R&nbsp;A&nbsp;C&nbsp;U&nbsp;L&nbsp;A', 'Dracula', 'DRACULA','Bram &nbsp; Stoker','Bram Stoker')
-replace_words = ('<strong>I&nbsp;Z&nbsp;I&nbsp;B&nbsp;I&nbsp;C&nbsp;K&nbsp;I</strong>', '<strong>Izibicki</strong>', '<strong>IZIBICKI</strong>','Eddie &nbsp; Shi','Eddie Shi')
+replace_words = ('<strong>I&nbsp;Z&nbsp;B&nbsp;I&nbsp;C&nbsp;K&nbsp;I</strong>', '<strong>Izbicki</strong>', '<strong>IZBICKI</strong>','Eddie &nbsp; Shi','Eddie Shi')
 
 
 for line in d: #checking every line in the html
@@ -20,6 +20,9 @@ for line in d: #checking every line in the html
   line = re.sub(r"\bCount\b", "Professor", line)
   line = re.sub(r"\bcount\b", "professor", line)
   line = re.sub(r"\bCOUNT\b", "PROFESSOR", line)
+  line = re.sub(r"\bCounts\b","Professors", line)
+  line = re.sub(r"\bcounts\b", "professors", line)
+  line = re.sub(r"\bCOUNTS\b", "PROFESSORS", line)
 
   i.write(line)
 
